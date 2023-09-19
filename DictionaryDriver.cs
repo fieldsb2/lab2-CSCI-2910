e9e01716c758d38dac50de08e8af9665b8f91027
fieldsb2 <112001235+fieldsb2@users.noreply.github.com>
2023-09-19 11:40:39 -0400

    Add files via upload

---
+++ DictionaryDriver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1___CSCI_2910
+{
+   internal class DictionaryDriver
+    {
+
+        static public void Main(string[] args)
+        {
+            List<VideoGame> ChooseGame = new List<VideoGame>();
+            string FilePath = "../../../../../videogames.csv";
+            StreamReader str = new StreamReader(FilePath);
+            str.ReadLine();
+            string LineInfo = "";
+            string name = "";
+            string plat = "";
+            int year = 0;
+            string pub = "";
+            string genre = "";
+            string nsale = "";
+            string esale = "";
+            string jsale = "";
+            string osale = "";
+            string gsale = "";
+
+            int position = 0;
+            int count = File.ReadAllLines(FilePath).Length;
+            count--;
+
+            while (count != 0)
+            {
+                LineInfo = str.ReadLine();
+                string[] games = LineInfo.Split(new string[] { "," }, StringSplitOptions.None);
+                name = games[position];
+                position++;
+                plat = games[position];
+                position++;
+                year = int.Parse(games[position]);
+                position++;
+                pub = games[position];
+                position++;
+                genre = games[position];
+                position++;
+                nsale = games[position];
+                position++;
+                esale = games[position];
+                position++;
+                jsale = games[position];
+                position++;
+                osale = games[position];
+                position++;
+                gsale = games[position];
+                position++;
+
+                count--;
+                VideoGame game1 = new VideoGame(name, plat, year, pub, genre, nsale, esale, jsale, osale, gsale);
+                                                                                                                 
+                ChooseGame.Add(game1);
+                position = 0;
+            }
+
+            Dictionary<string, List<VideoGame>> myDictionary = ChooseGame.GroupBy(o => o.Platform).ToDictionary(g => g.Key, g => g.OrderByDescending(g => g.G_Sales).Take(5).ToList());
+
+
+            foreach (var game in myDictionary)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(game.Key);
+                Console.WriteLine("----------------------------------------: \n");
+                for (int i = 0; i < game.Value.Count; i++)
+                {
+                    Console.WriteLine(game.Value[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: Lab 1 - Video Games
+// File Name: Program.cs
+// Description: A class that implements a video game list and manipulates it, while also taking in user commands.
+// Course: CSCI 2910-001 – Server Side Web Programming
+// Author: Braydon Fields, fieldsb2@etsu.edu, East Tennessee State University
+// Created: Thursday, September 1, 2023
+// Copyright: Braydon Fields, 2023
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+
+/// <summary>
+/// a class for a video game that implements IComparable
+/// </summary>
+public class VideoGame : IComparable<VideoGame>
+{
+    public string Name;// the video game name
+    public string Platform;// the video game platform
+    public int Year;// the video game year
+    public string Genre; // the video game genre
+    public string Publisher;// the video game publisher
+    public string NA_Sales;// north american sales
+    public string EU_Sales;// european sales
+    public string JP_Sales;// japanese sales
+    public string O_Sales;// other sales
+    public string G_Sales;// global sales
+
+    /// <summary>
+    /// a video game class that takes no parameters
+    /// </summary>
+
+
+    /// <summary>
+    /// a class that takes in all parameters for a video game object
+    /// </summary>
+    /// <param name="name">name of video game</param>
+    /// <param name="platform">platform of video game</param>
+    /// <param name="year">year video game was made</param>
+    /// <param name="genre">genre of video game</param>
+    /// <param name="publisher">publisher of video game</param>
+    /// <param name="na_Sales">north american sales</param>
+    /// <param name="eu_Sales">european sales</param>
+    /// <param name="jp_Sales">japanese sales</param>
+    /// <param name="o_Sales">other sales</param>
+    /// <param name="g_Sales">global sales</param>
+    public VideoGame(string name, string platform, int year, string genre, string publisher, string na_Sales, string eu_Sales, string jp_Sales, string o_Sales, string g_Sales)
+    {
+        Name = name;
+        Platform = platform;
+        Year = year;
+        Genre = genre;
+        Publisher = publisher;
+        NA_Sales = na_Sales;
+        EU_Sales = eu_Sales;
+        JP_Sales = jp_Sales;
+        O_Sales = o_Sales;
+        G_Sales = g_Sales;
+    }
+
+    /// <summary>
+    /// an implemented method from IComparable that allows lists to be sorted
+    /// </summary>
+    /// <param name="game1">the game it takes in to sort by name</param>
+    /// <returns>an int that determines which name came first</returns>
+    public int CompareTo(VideoGame? game1)
+    {
+        int CName = Name.CompareTo(game1.Name);
+        return CName;
+
+    }
+
+    /// <summary>
+    /// a method that returns the publisher of the video game
+    /// </summary>
+    /// <returns>publisher of video game</returns>
+    public string GetPublisher()
+    {
+        return Publisher;
+    }
+
+    /// <summary>
+    /// a method that returns the genre of the video game
+    /// </summary>
+    /// <returns>genre of video game</returns>
+    public string GetGenre()
+    {
+        return Genre;
+    }
+
+    public string GetPlatform()
+    {
+        return Platform;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    /// <summary>
+    /// a method that takes in what publisher the user enters and returns it
+    /// </summary>
+    /// <returns>a string containing user input</returns>
+    public string PublisherData()
+    {
+        string userInput = "";
+        userInput = Console.ReadLine();
+        return userInput;
+    }
+
+    /// <summary>
+    /// a method that takes in what genre the user enters and returns it
+    /// </summary>
+    /// <returns>string containing user input</returns>
+    public string GenreData()
+    {
+        string userInput = "";
+        userInput = Console.ReadLine();
+        return userInput;
+    }
+
+    /// <summary>
+    /// a method that returns a string of the video game name, publisher, and genre
+    /// </summary>
+    /// <returns>a string containing name, publisher, and genre</returns>
+    public override string ToString()
+    {
+        string value = "";
+        value = Name;
+        return value;
+    }
+}
